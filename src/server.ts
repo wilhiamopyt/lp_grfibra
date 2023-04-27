b63f61cbb7de67f20e7adf72ddda7c89a3f002fd
mac-rodrigo <rodrigo.growth.opyt@gmail.com>
2023-04-27 16:25:41 -0300

    V1 finalizada

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ router.use(cors());
 
 router.use(express.urlencoded({ extended: false }));
 router.use(express.json());
+router.use(express.static('public'));
 
 router.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -19,6 +20,14 @@ router.use((req, res, next) => {
 router.use(rotas);
 
 const PORT = 6060;
+
+router.use("/", (req, res) => {
+  // console.log(`${__dirname}/../index.html`);
+
+  // const fullFile = __dirname.split("/src/")[0];
+
+  return res.sendFile(`${__dirname}/index.html`)
+})
 
 router.listen(PORT, () => {
   console.log(`Servidor está executando na porta ${PORT}`);
